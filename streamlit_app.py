b2325eb9fab1cb5cac7ac410b8740c3c7b6f6a0e
lbailey-NAS <lucy.bailey@nas.org.uk>
2026-06-08 15:05:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,10 +20,6 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on the Smoothie will be: ", name_on_order)
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-# st.text(smoothiefroot_response.json())
-st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-
 ingredients_list = st.multiselect('Choose up to 5 ingredients:',
                                   my_dataframe,
                                   max_selections=5);
@@ -33,6 +29,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # st.write(ingredients_string)
 
