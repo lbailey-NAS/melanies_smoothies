b3a9e4d22edd8d4452dd891a824881d24722aad9
lbailey-NAS <lucy.bailey@nas.org.uk>
2026-06-08 15:02:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,8 @@ name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on the Smoothie will be: ", name_on_order)
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response.json())
+# st.text(smoothiefroot_response.json())
+st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients:',
                                   my_dataframe,
